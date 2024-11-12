759b422fda56ce8f4a59b71d3696c5a15dc3fa90
Verenisse Famara Cáceres Pérez <veref.caceres@gmail.com>
2024-11-12 21:04:46 +0000

    General modifications

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Verenisse Famara Cáceres Pérez",
+  title: "Computer Science Student",
+  email: "veref.caceres@gmail.com",
+  gitHub: "vef-am",
+  instagram: "",
+  linkedIn: "verenisse-famara-cáceres-pérez",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
@@ -43,7 +43,15 @@ const App = () => {
     <div id="main">
       <Header />
       <Home name={siteProps.name} title={siteProps.title} />
-      <About />
+      <div
+        style={{
+          backgroundColor: "black",
+          backgroundSize: "cover",
+          backgroundBlendMode: "overlay",
+        }}
+      >
+        <About />
+      </div>
       <Portfolio />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/markus-spiske-iar-afB0QQw-unsplash.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a third-year Computer Science & Engineering student at the Polytechnic University of Catalonia (UPC), specializing in Computing. I enjoy solving problems.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C++",
+  "C",
+  "Java",
+  "Visual Studio",
+  "SQL",
+  "PostgreSQL",
 ];
 
 /**
@@ -45,23 +45,36 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about technology, and I am always looking forward to learning and developing my skills further. I am interested in opportunities that let me apply my knowledge and contribute to the success of a company.";
 
 const About = () => {
   return (
-    <section className="padding" id="about">
-      <img className="background" src={image} alt={imageAltText} />
+    <section className="padding" id="about"
+    style={{
+      marginTop: "2rem",
+      marginBottom: "2rem",
+      //padding: "2rem",
+      backgroundImage: `url(${image})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      position: "relative",
+      //boxSizing: "border-box", // Asegura que el padding se incluya en el tamaño total
+    }}>
+      {/* <img className="background" src={image} alt={imageAltText} /> */}
       <div
         style={{
-          backgroundColor: "white",
+          backgroundColor: "black",
           width: "50%",
           padding: "4rem",
           margin: "3rem auto",
           textAlign: "center",
+          borderRadius: "10px",
+          border: "1px solid green",
         }}
       >
         <h2>About Myself</h2>
-        <p className="large">{description}</p>
+        <p className="large" style={{ color: "#efefef" }}>{description}</p>
         <hr />
         <ul
           style={{
@@ -73,11 +86,15 @@ const About = () => {
           }}
         >
           {skillsList.map((skill) => (
-            <li key={skill}>{skill}</li>
+            <li
+             style={{
+              color: "white",
+             }}
+             key={skill}>{skill}</li>
           ))}
         </ul>
         <hr />
-        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+        <p style={{ padding: "1rem 3rem 0", color: "#efefef" }}>{detailOrQuote}</p>
       </div>
     </section>
   );
